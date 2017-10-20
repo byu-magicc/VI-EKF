6f77b67445a00d26590040f33efb00cfaf25f3d0
Robert Pottorff <rpottorff@gmail.com>
2017-10-20 16:12:19 -0600

    made state a nx1 matrix instead of vector, load images filenames, and unwarp images

--- python/data_loader.py
+++ python/data_loader.py
@@ -5,6 +5,7 @@ sys.path.append('/usr/local/lib/python2.7/site-packages')
 import cv2
 from tqdm import tqdm
 from quaternion import Quaternion
+import yaml
 
 R_NWU_NED = np.array([[1, 0, 0],
                       [0, -1, 0],
@@ -19,6 +20,21 @@ def load_from_file(filename):
 
 def save_to_file(filename, data):
     np.save(filename, data)
+
+
+def make_undistort_funtion(intrinsics, resolution, distortion_coefficients):
+    A = np.array([[float(intrinsics[0]), 0., float(intrinsics[2])], [0., float(intrinsics[1]), float(intrinsics[3])], [0., 0., 1.]])
+
+    print(distortion_coefficients, distortion_coefficients.dtype)
+
+    Ap, _ = cv2.getOptimalNewCameraMatrix(A, distortion_coefficients, (resolution[0], resolution[1]), 1.0)
+
+    print(Ap)
+
+    def undistort(image):
+        return cv2.undistort(image, A, distortion_coefficients, None, Ap)
+
+    return undistort
 
 def load_data(folder, show_image=False, start=0, end=-1):
     # First, load IMU data
@@ -42,7 +58,8 @@ def load_data(folder, show_image=False, start=0, end=-1):
     imu_data[:,0] /= 1e9
 
     # Load Camera Data
-    # images = []
+    images0 = []
+    images1 = []
     image_time = []
     csvfile = open(folder + '/cam0/data.csv', 'rb')
     reader = csv.reader(csvfile)
@@ -50,11 +67,12 @@ def load_data(folder, show_image=False, start=0, end=-1):
     for i, row in tqdm(enumerate(reader)):
         if i > 0:
             image_time.append((float(row[0]) - t0) / 1e9)
-            # images.append(cv2.imread(folder+'/cam0/data/' + row[1]))
+            images0.append(folder + '/cam0/data/' + row[1])
+            images1.append(folder + '/cam1/data/' + row[1])
             if show_image:
                 cv2.imshow('image', cv2.imread(folder+'/cam0/data/' + row[1]))
                 print image_time[-1]
-                cv2.waitKey(0)
+                #cv2.waitKey(0)
     image_time = np.array(image_time)
     # images = np.array(images)
 
@@ -81,19 +99,57 @@ def load_data(folder, show_image=False, start=0, end=-1):
 
     # chop data
     imu_data = imu_data[imu_data[:,0] > start, :]
+    images0 = [f for f, t in zip(images0, image_time > start) if t]
+    images1 = [f for f, t in zip(images1, image_time > start) if t]
     image_time = image_time[image_time > start]
     ground_truth = ground_truth[ground_truth[:, 0] > start, :]
 
     if end > start:
         imu_data = imu_data[imu_data[:, 0] < end, :]
-        image_time = image_time[image_time < end]
         ground_truth = ground_truth[ground_truth[:, 0] < end, :]
+        images0 = [f for f, t in zip(images0, image_time < end) if t]
+        images1 = [f for f, t in zip(images1, image_time < end) if t]
+        image_time = image_time[image_time < end]
 
     print "done reading data"
 
+    with open(folder + '/cam0/sensor.yaml', 'r') as stream:
+        try:
+            data = yaml.load(stream)
+
+            print(data['intrinsics'])
+            cam0_sensor = {
+                'resolution': np.array(data['resolution']),
+                'intrinsics': np.array(data['intrinsics']),
+                'rate_hz': data['rate_hz'],
+                'distortion_coefficients': np.array(data['distortion_coefficients']),
+                'body_to_sensor_transform': np.array(data['T_BS']['data']).reshape((data['T_BS']['rows'],data['T_BS']['cols']))
+            }
+
+
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    with open(folder + '/cam1/sensor.yaml', 'r') as stream:
+        try:
+            data = yaml.load(stream)
+            cam1_sensor = {
+                'resolution': data['resolution'],
+                'intrinsics': data['intrinsics'],
+                'rate_hz': data['rate_hz'],
+                'distortion_coefficients': np.array(data['distortion_coefficients']),
+                'body_to_sensor_transform': np.array(data['T_BS']['data']).reshape((data['T_BS']['rows'],data['T_BS']['cols']))
+            }
+
+        except yaml.YAMLError as exc:
+            print(exc)
+
     out_dict = dict()
     out_dict['imu'] = imu_data
-    # out_dict['cam'] = images
+    out_dict['cam0_sensor'] = cam0_sensor
+    out_dict['cam1_sensor'] = cam1_sensor
+    out_dict['cam0_frame_filenames'] = images0
+    out_dict['cam1_frame_filenames'] = images1
     out_dict['cam_time'] = image_time
     out_dict['truth'] = ground_truth
     return out_dict
@@ -101,6 +157,6 @@ def load_data(folder, show_image=False, start=0, end=-1):
 
 
 if __name__ == '__main__':
-    data = load_data('data/mav0', show_image=True)
-    save_to_file('data/mav0/data.npy', data)
+    data = load_data('/mnt/pccfs/not_backed_up/eurocmav/mav0', show_image=True)
+    save_to_file('/mnt/pccfs/not_backed_up/eurocmav/mav0/data.npy', data)
     print "done"
--- python/vi_ekf.py
+++ python/vi_ekf.py
@@ -1,6 +1,7 @@
 import numpy as np
 from quaternion import Quaternion
 import scipy.linalg
+import cv2
 
 # State: pos, vel, att, b_gyro, b_acc, mu
 class VI_EKF():
@@ -20,23 +21,29 @@ class VI_EKF():
         self.khat = np.array([[0, 0, 1]]).T
 
     def boxplus(self, x, dx):
-        out = np.zeros(17)
+        assert x.shape == (17, 1) and dx.shape == (16, 1)
+
+        out = np.zeros((17, 1))
         out[0:6] = x[0:6] + dx[0:6]
         quat = Quaternion(x[6:10])
-
         norm_delta = scipy.linalg.norm(dx[6:9])
+
         if norm_delta > 1e-4:
             dquat = Quaternion(scalar=np.cos(norm_delta/2.), vector=np.sin(norm_delta/2.)*dx[6:9]/norm_delta)
-            out[6:10] = (quat * dquat).elements
+            out[6:10, 0] = (quat * dquat).elements
         else:
-            dquat = Quaternion(scalar = 1., vector=dx[6:9]/2.)
-            out[6:10] = (quat * dquat).unit.elements
+            dquat = Quaternion(scalar=1., vector=dx[6:9]/2.)
+            out[6:10, 0] = (quat * dquat).unit.elements
+
         out[10:] = x[10:] + dx[9:]
         return out
 
     def propagate(self, y_acc, y_gyro, dt):
+        assert y_acc.shape == (3, 1) and y_gyro.shape == (3, 1)
+
         xdot = self.f(self.x, y_acc, y_gyro)
         self.x = self.boxplus(self.x, xdot*dt)
+
         return self.x.copy()
 
     def set_imu_bias(self, b_g, b_a):
@@ -44,54 +51,67 @@ class VI_EKF():
         self.x[13:16] = b_a
 
     def f(self, x, y_acc, y_gyro):
+        assert x.shape == (17, 1) and y_acc.shape == (3,1) and y_gyro.shape == (3,1)
+
         vel = x[3:6]
         q_I_b = Quaternion(x[6:10])
 
         omega = y_gyro - x[10:13]
         acc = y_acc - x[13:16]
-        mu = x[16]
+        mu = x[16, None]
 
-        pdot = np.atleast_2d(q_I_b.rotate(vel))
-        vdot = np.cross(vel, omega) + np.atleast_2d(acc) + np.atleast_2d(q_I_b.inverse.rotate(self.gravity))
-        qdot = np.array([omega])
+        vdot = np.cross(vel, omega, axis=0) + acc + q_I_b.inverse.rotate(self.gravity)[:, None]
+        qdot = omega
+        pdot = q_I_b.rotate(vel)[:, None]
 
-        return np.concatenate((pdot, vdot, qdot, np.zeros((1,7))), axis=1).flatten()
+        return np.vstack((pdot, vdot, qdot, np.zeros((7, 1))))
 
 if __name__ == '__main__':
     from data_loader import *
     from mpl_toolkits.mplot3d import Axes3D
     import matplotlib.pyplot as plt
-
+    import tqdm
 
     # Load Data
-    # data = load_data('data/mav0', show_image=False, start=16, end=30)
-    # save_to_file('data/mav0/data.npy', data)
-    data = load_from_file('data/mav0/data.npy')
+    data = load_data('/mnt/pccfs/not_backed_up/eurocmav/mav0', show_image=False, start=16, end=30)
+    save_to_file('/mnt/pccfs/not_backed_up/eurocmav/mav0/data.npy', data)
+    # data = load_from_file('/mnt/pccfs/not_backed_up/eurocmav/mav0/data.npy')
 
-    imu_t = data['imu'][:,0]
-    gyro = data['imu'][:,1:4]
-    acc = data['imu'][:,4:7]
+    imu_t = data['imu'][:, 0]
+    gyro = data['imu'][:, 1:4]
+    acc = data['imu'][:, 4:7]
+    cam_time = data['cam_time']
+
+    cam0_undistort = make_undistort_funtion(intrinsics=data['cam0_sensor']['intrinsics'], resolution=data['cam0_sensor']['resolution'], distortion_coefficients=data['cam0_sensor']['distortion_coefficients'])
+    cam1_undistort = make_undistort_funtion(intrinsics=data['cam0_sensor']['intrinsics'], resolution=data['cam0_sensor']['resolution'], distortion_coefficients=data['cam0_sensor']['distortion_coefficients'])
 
     truth = data['truth']
 
-    x0 = np.concatenate([truth[0,1:4,None], truth[0,8:11,None], truth[0,4:8,None], np.zeros((7,1))], axis=0)
+    x0 = np.vstack([truth[0, 1:4, None], truth[0, 8:11, None], truth[0, 4:8, None], np.zeros((7, 1))])
 
     ekf = VI_EKF(x0)
 
     estimate = []
+    measurement_index = 0
 
-    truth_index = 0
-    prev_time = imu_t[0].copy()
-    imu_t = np.delete(imu_t, 0)
-    for i, t in enumerate(imu_t):
-        while truth[truth_index,0] < t and truth[truth_index,0] > prev_time:
-            ekf.set_imu_bias(truth[11:14], truth[14:17])
-        x = ekf.propagate(acc[i,:,None], gyro[i,:,None], t - prev_time)
-        estimate.append(x.copy().flatten())
-        prev_time = t
+    for i, (t, dt) in enumerate(tqdm.tqdm(zip(imu_t, np.diff(np.concatenate([[0], imu_t]))))):
+
+        if measurement_index < cam_time.shape[0] and t > cam_time[measurement_index]:
+            print(data['cam0_frame_filenames'][measurement_index])
+            image = cv2.imread(data['cam0_frame_filenames'][measurement_index])
+            image_undistort = cam0_undistort(image)
+
+            measurement_index += 1
+
+        #while truth[truth_index,0] < t and truth[truth_index,0] > prev_time:
+        #    ekf.set_imu_bias(truth[i, 11:14, None], truth[i, 14:17, None])
+        x = ekf.propagate(acc[i, :, None], gyro[i, :, None], dt)
+
+        estimate.append(x[:, 0])
+
+    print(measurement_index, cam_time.shape[0])
 
     estimate = np.array(estimate)
-
 
     truth_t = truth[:,0]
 

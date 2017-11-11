27cf94c1ebbae8bae98732f695f007285372a06b
Robert Pottorff <rpottorff@gmail.com>
2017-11-10 17:32:31 -0700

    htests

--- vi_ekf/post_process_bag.py
+++ vi_ekf/post_process_bag.py
@@ -29,7 +29,7 @@ x0 = np.concatenate([data['truth_NED']['pos'][truth_start_index,:,None],
 ekf = VI_EKF(x0)
 
 
-for i in range(0):
+for i in range(1):
     ekf.init_feature(data['features']['zeta'][truth_start_index,i,:,None], data['features']['depth'][truth_start_index,i])
 
 prev_time = 0
@@ -66,17 +66,37 @@ print '\ndxVEL:dxB_A error\n', d_dfdx[dxVEL:dxVEL+3, dxB_A:dxB_A+3] - a_dfdx[dxV
 print '\ndxVEL:dxB_G error\n', d_dfdx[dxVEL:dxVEL+3, dxB_G:dxB_G+3] - a_dfdx[dxVEL:dxVEL+3, dxB_A:dxB_A+3]
 print '\ndxVEL:dxMU error\n', d_dfdx[dxVEL:dxVEL+3, dxMU, None]    - a_dfdx[dxVEL:dxVEL+3, dxB_G:dxB_G+3]
 
-
 a_dfdu = ekf.dfdu(x)
 d_dfdu = np.zeros_like(a_dfdu)
 I = np.eye(d_dfdu.shape[1])
-epsilon = 1e-15
 for i in range(d_dfdu.shape[1]):
     u_prime = u + (I[i] * epsilon)[:, None]
     d_dfdu[:, i] = ((ekf.f(x, u_prime) - ekf.f(x, u)) / epsilon)[:, 0]
 
 print '\ndxVEL:uA error\n', a_dfdu[dxVEL:dxATT, uA] - d_dfdu[dxVEL:dxATT, uA]
 print '\ndxVEL:uG error\n', a_dfdu[dxVEL:dxATT, uG:] - d_dfdu[dxVEL:dxATT, uG:]
+
+def htest(fn, **kwargs):
+    print '\nTesting ', fn.__name__
+    try:
+        analytical = fn(x, **kwargs)[1]
+        finite_difference = np.zeros_like(analytical)
+        I = np.eye(finite_difference.shape[1])
+        epsilon = 1e-5
+        for i in range(finite_difference.shape[1]):
+            x_prime = ekf.boxplus(x, (I[i] * epsilon)[:, None])
+            finite_difference[:, i] = ((fn(x_prime, **kwargs)[0] - fn(x, **kwargs)[0]) / epsilon)[:, 0]
+
+        print analytical - finite_difference
+    except Exception as e:
+        print 'error:', e
+
+htest(ekf.h_acc)
+htest(ekf.h_alt)
+htest(ekf.h_feat, i=0)
+htest(ekf.h_depth, i=0)
+htest(ekf.h_inv_depth, i=0)
+htest(ekf.h_pixel_vel, i=0, u=u)
 
 quit()
 

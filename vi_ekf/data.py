updated math, fixed jacobian

vi_ekf/data.py
@@ -15,11 +15,11 @@ class Data(object):
         self.R = {'alt': 0.01,
                   'acc': np.diag([0.5, 0.5]),
                   'att': np.diag([0.01, 0.01, 0.01]),
-                  'vel': np.diag([0.0001, 0.0001, 0.0001]),
+                  'vel': np.diag([0.01, 0.01, 0.01]),
                   'pos': np.diag([0.1, 0.1, 0.1]),
                   'zeta': np.diag([0.01, 0.01]),
                   'lambda': np.diag([1., 1.]),
-                  'depth': 0.01}
+                  'depth': 0.1}
 
     def indexer(self, target_time, source_time):
         index_for_target = []

vi_ekf/main.py
@@ -6,9 +6,11 @@ from data import FakeData, ETHData, ROSbagData, History
 from pyquat import Quaternion, quat_arr_to_euler
 from plot_helper import plot_3d_trajectory
 
+start = 25.0
+end = 30.0
 # data = ETHData(filename='/mnt/pccfs/not_backed_up/eurocmav/V1_01_easy/mav0', start=5.0, end=120.0, sim_features=True, load_new=True)
 # data = ROSbagData(filename='data/truth_imu_flight.bag', start=30.0, end=68.0, sim_features=True, load_new=True)
-data = ROSbagData(filename='data/truth_imu_depth_mono.bag', start=8.0, end=9.0, sim_features=False, load_new=False)
+data = ROSbagData(filename='data/xtion_collect.bag', start=start, end=end, sim_features=False, load_new=True)
 data.__test__()
 
 ekf = viekf.VI_EKF(data.x0)
@@ -25,13 +27,15 @@ for i, (t, pos, vel, att, gyro, acc, lambdas, depths, ids) in enumerate(tqdm(dat
     if acc is not None and gyro is not None:
         x_hat, P = ekf.propagate(acc, gyro, t)
         h.store(t, x_hat=x_hat[:viekf.xZ], P=P[:viekf.dxZ, :viekf.dxZ])
+        vel_c_i, omega_c_i = ekf.get_camera_state()
+        h.store(t, vel_c_i=vel_c_i, omega_c_i=omega_c_i)
 
     # sensor updates - save off residual information
     if pos is not None:
         h.store(t, alt_res=ekf.update(-pos[2], 'alt', data.R['alt'], passive=True)[0])
-        h.store(t, att_res=ekf.update(att, 'att', data.R['att'], passive=False)[0],
-                pos_res=ekf.update(pos, 'pos', data.R['pos'], passive=False)[0],
-                vel_res=ekf.update(vel, 'vel', data.R['vel'], passive=False)[0])
+        h.store(t, att_res=ekf.update(att, 'att', data.R['att'], passive=True)[0],
+                pos_res=ekf.update(pos, 'pos', data.R['pos'], passive=True)[0],
+                vel_res=ekf.update(vel, 'vel', data.R['vel'], passive=True)[0])
 
     if acc is not None:
         h.store(t, acc_res=ekf.update(acc[:2], 'acc', data.R['acc'], passive=True)[0])
@@ -39,37 +43,41 @@ for i, (t, pos, vel, att, gyro, acc, lambdas, depths, ids) in enumerate(tqdm(dat
     # feature updates
     if ids is not None and len(ids) > 0:
         ekf.keep_only_features(ids)
-        for l, depth, id in zip(lambdas, depths, ids):
-            lambda_res, lambda_hat = ekf.update(l, 'feat', data.R['lambda'], passive=False, i=id)
+        for j, (l, depth, id) in enumerate(zip(lambdas, depths, ids)):
+            lambda_res, lambda_hat = ekf.update(l, 'feat', data.R['lambda'], passive=False, i=id, depth=depth)
             depth_res, depth_hat = ekf.update(depth, 'depth', data.R['depth'], passive=np.isnan(depth), i=id)
-            h.store(t, id, depth_hat=depth_hat, depth=depth, depth_res=depth_res)
-            h.store(t, id, lambda_res=lambda_res, lambda_hat=lambda_hat, lmda=l)
-            dxRHO_i = viekf.dxZ+3*ekf.global_to_local_feature_id[id]+2
-            h.store(t, id, Pfeat=ekf.P[dxRHO_i, dxRHO_i,None,None])
+            if j < 5:
+                h.store(t, id, depth_hat=depth_hat, depth=depth, depth_res=depth_res)
+                h.store(t, id, lambda_res=lambda_res, lambda_hat=lambda_hat, lmda=l)
+                dxRHO_i = viekf.dxZ+3*ekf.global_to_local_feature_id[id]+2
+                h.store(t, id, Pfeat=ekf.P[dxRHO_i, dxRHO_i,None,None])
 
 # plot
 if True:
     # convert all our linked lists to contiguous numpy arrays and initialize the plot parameters
     h.tonumpy()
-    init_plots()
+    init_plots(start, end)
 
     plot_side_by_side('x_pos', viekf.xPOS, viekf.xPOS+3, h.t.x_hat, h.x_hat, cov=h.P, truth_t=h.t.pos, truth=h.pos, labels=['x', 'y', 'z'])
     plot_side_by_side('x_vel', viekf.xVEL, viekf.xVEL+3, h.t.x_hat, h.x_hat, cov=h.P, truth_t=h.t.vel, truth=h.vel, labels=['x', 'y', 'z'])
     # plot_side_by_side('x_att', viekf.xATT, viekf.xATT+4, h.t.x_hat, h.x_hat, truth_t=h.t.att, truth=h.att, labels=['w', 'x', 'y', 'z'])
 
     plot_side_by_side('x_euler', 0, 3, h.t.x_hat, quat_arr_to_euler(h.x_hat[:, viekf.xATT:viekf.xATT+4, 0].T).T, truth_t=h.t.att, truth=quat_arr_to_euler(h.att.T[0]).T, labels=[r'$\phi$', r'$\rho$', r'$\psi$'])
-    plot_side_by_side('x_b_g', viekf.xB_G, viekf.xB_G + 3, h.t.x_hat, h.x_hat, cov=h.P, labels=['x', 'y', 'z'], cov_bounds=(viekf.dxB_G,viekf.dxB_G+3))
-    plot_side_by_side('x_b_a', viekf.xB_A, viekf.xB_A + 3, h.t.x_hat, h.x_hat, cov=h.P, labels=['x', 'y', 'z'], cov_bounds=(viekf.dxB_A,viekf.dxB_A+3))
-    plot_side_by_side('x_mu', viekf.xMU, viekf.xMU+1, h.t.x_hat, h.x_hat, cov=h.P, labels=['mu'], cov_bounds=(viekf.dxMU,viekf.dxMU+1))
+    # plot_side_by_side('x_b_g', viekf.xB_G, viekf.xB_G + 3, h.t.x_hat, h.x_hat, cov=h.P, labels=['x', 'y', 'z'], cov_bounds=(viekf.dxB_G,viekf.dxB_G+3))
+    # plot_side_by_side('x_b_a', viekf.xB_A, viekf.xB_A + 3, h.t.x_hat, h.x_hat, cov=h.P, labels=['x', 'y', 'z'], cov_bounds=(viekf.dxB_A,viekf.dxB_A+3))
+    # plot_side_by_side('x_mu', viekf.xMU, viekf.xMU+1, h.t.x_hat, h.x_hat, cov=h.P, labels=['mu'], cov_bounds=(viekf.dxMU,viekf.dxMU+1))
 
-    plot_side_by_side('z_alt_residual', 0, 1, h.t.alt_res, h.alt_res, labels=['z_alt_res'])
-    plot_side_by_side('z_att_residual', 0, 3, h.t.att_res, h.att_res, labels=['x', 'y', 'z'])
-    plot_side_by_side('z_acc_residual', 0, 2, h.t.acc_res, h.acc_res, labels=['x', 'y'])
-    plot_side_by_side('z_pos_residual', 0, 3, h.t.pos_res, h.pos_res, labels=['x', 'y', 'z'])
-    plot_side_by_side('z_vel_residual', 0, 3, h.t.vel_res, h.vel_res, labels=['x', 'y', 'z'])
+    # plot_side_by_side('z_alt_residual', 0, 1, h.t.alt_res, h.alt_res, labels=['z_alt_res'])
+    # plot_side_by_side('z_att_residual', 0, 3, h.t.att_res, h.att_res, labels=['x', 'y', 'z'])
+    # plot_side_by_side('z_acc_residual', 0, 2, h.t.acc_res, h.acc_res, labels=['x', 'y'])
+    # plot_side_by_side('z_pos_residual', 0, 3, h.t.pos_res, h.pos_res, labels=['x', 'y', 'z'])
+    # plot_side_by_side('z_vel_residual', 0, 3, h.t.vel_res, h.vel_res, labels=['x', 'y', 'z'])
 
     plot_side_by_side('u_gyro', 0, 3, h.t.gyro, h.gyro, labels=['x', 'y', 'z'])
-    plot_side_by_side('u_acc', 0, 3, h.t.acc, h.acc, labels=['x', 'y', 'z'])
+    # plot_side_by_side('u_acc', 0, 3, h.t.acc, h.acc, labels=['x', 'y', 'z'])
+
+    plot_side_by_side('camera_vel', 0, 3, h.t.vel_c_i, h.vel_c_i, labels=['x','y','z'])
+    plot_side_by_side('camera_omega', 0, 3, h.t.omega_c_i, h.omega_c_i, labels=['x', 'y', 'z'])
 
     ids = []
     for step_ids in h.ids:
@@ -81,15 +89,15 @@ if True:
         plot_side_by_side('lambda_{}'.format(i), 0, 2, h.t.lambda_hat[i], h.lambda_hat[i], truth_t=h.t.lmda[i], truth=h.lmda[i], labels=['u','v'])
         # plot_side_by_side('x_feat_{}'.format(i), 0, 3, h.t.zeta_hat[i], h.zeta_hat[i], truth_t=h.t.zeta[i], truth=h.zeta[i], labels=['x', 'y', 'z'])
         # plot_side_by_side('x_qfeat_{}'.format(i), 0, 4, h.t.qzeta_hat[i], h.qzeta_hat[i], truth_t=h.t.qzeta[i], truth=h.qzeta[i], labels=['w','x', 'y', 'z'])
+        # plot_side_by_side('lambda_{}_residual'.format(i), 0, 2, h.t.lambda_res[i], h.lambda_res[i], labels=['x', 'y'])
 
         if i in h.depth.keys():
-            plot_side_by_side('x_rho_{}'.format(i), 0, 1, h.t.depth_hat[i], h.depth_hat[i], cov=h.Pfeat[i], truth_t=h.t.depth[i], truth=h.depth[i], labels=[r'$\frac{1}{\rho}$'])
+            plot_side_by_side('x_rho_{}'.format(i), 0, 1, h.t.depth_hat[i], h.depth_hat[i], truth_t=h.t.depth[i], truth=h.depth[i], labels=[r'$\frac{1}{\rho}$'])
         else:
-            plot_side_by_side('x_rho_{}'.format(i), 0, 1, h.t.depth_hat[i], h.depth_hat[i], cov=h.Pfeat[i], labels=[r'$\frac{1}{\rho}$'])
-        #
-        #
-        # plot_side_by_side('z_zeta_{}_residual'.format(i), 0, 2, h.t.zeta_res[i], h.zeta_res[i], labels=['x', 'y'])
-        # plot_side_by_side('z_depth_{}_residual'.format(i), 0, 1, h.t.depth_res[i], h.depth_res[i], labels=['rho'])
+            plot_side_by_side('x_rho_{}'.format(i), 0, 1, h.t.depth_hat[i], h.depth_hat[i], labels=[r'$\frac{1}{\rho}$'])
+            #
+            #
+            # plot_side_by_side('z_depth_{}_residual'.format(i), 0, 1, h.t.depth_res[i], h.depth_res[i], labels=['rho'])
 
 
 

vi_ekf/plot_helper.py
@@ -6,13 +6,20 @@ from mpl_toolkits.mplot3d import Axes3D
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection, Line3DCollection
 import os
 
-def init_plots():
+plot_start = None
+plot_end = None
+
+def init_plots(start, end):
     # Set the colormap to 'jet'
     plt.jet()
     plt.set_cmap('jet')
     plt.rcParams['image.cmap'] = 'jet'
     plt.rcParams['figure.max_open_warning'] = 100
     os.system("rm plots/*.png")
+
+    global plot_start, plot_end
+    plot_start = start
+    plot_end = end
 
 def get_colors(num_rows, cmap):
     return cmap(np.linspace(0, 1, num_rows))
@@ -47,15 +54,20 @@ def plot_side_by_side(title, start, end, est_t, estimate, cov=None, truth_t=None
         plt.plot(est_t[::skip], estimate[::skip, i], label=labels[i] + 'hat')
 
         if isinstance(cov, np.ndarray):
-            plt.plot(est_t[::skip], estimate[::skip, i].flatten() + 2 * cov_copy[::skip, i, i].flatten(), 'k-', alpha=0.5)
-            plt.plot(est_t[::skip], estimate[::skip, i].flatten() - 2 * cov_copy[::skip, i, i].flatten(), 'k-', alpha=0.5)
+            try:
+                plt.plot(est_t[::skip], estimate[::skip, i].flatten() + 2 * cov_copy[::skip, i, i].flatten(), 'k-', alpha=0.5)
+                plt.plot(est_t[::skip], estimate[::skip, i].flatten() - 2 * cov_copy[::skip, i, i].flatten(), 'k-', alpha=0.5)
+            except:
+                debug = 1
 
+        plt.xlim(plot_start, plot_end)
         plt.legend()
         if i == 0:
             plt.title(title)
 
     if save:
         plt.savefig('plots/'+title+'.png')
+        plt.close()
 
 def plot_cube(q_I_b, zetas, zeta_truth):
 

vi_ekf/rosbag_data_loader.py
@@ -9,12 +9,14 @@ from add_landmark import add_landmark
 import yaml
 import matplotlib.pyplot as plt
 import rosbag
+import rospy
 from mpl_toolkits.mplot3d import Axes3D
 import scipy.signal
 from plot_helper import plot_3d_trajectory
 import klt_tracker
 import struct
 import scipy.interpolate
+import cPickle
 
 def to_list(vector3):
     return [vector3.x, vector3.y, vector3.z]
@@ -86,67 +88,74 @@ def bilinear_interpolate(im, x, y):
 
 
 def load_data(filename, start=0, end=np.inf, sim_features=False, show_image=False, plot_trajectory=True):
-    print "loading rosbag", filename
-    # First, load IMU data
-    bag = rosbag.Bag(filename)
-    imu_data = []
-    truth_pose_data = []
-    image_data = []
-    depth_data = []
-    image_time = []
-    depth_time = []
-    #bridge = CvBridge()
 
-    topic_list = ['/imu/data',
-                  '/vrpn_client_node/Leo/pose',
-                  '/vrpn/Leo/pose',
-                  '/baro/data',
-                  '/sonar/data',
-                  '/is_flying',
-                  '/gps/data',
-                  '/mag/data',
-                  '/camera/rgb/image_raw/compressed',
-                  '/camera/depth/image/compressedDepth']
+    if not os.path.isfile('data/bag_tmp.pkl'):
+        print "loading rosbag", filename
+        # First, load IMU data
+        bag = rosbag.Bag(filename)
+        imu_data = []
+        truth_pose_data = []
+        image_data = []
+        depth_data = []
+        image_time = []
+        depth_time = []
 
-    for topic, msg, t in tqdm(bag.read_messages(topics=topic_list), total=bag.get_message_count(topic_list) ):
-        if topic == '/imu/data':
-            imu_meas = [msg.header.stamp.to_sec(),
-                        msg.angular_velocity.x, msg.angular_velocity.y, msg.angular_velocity.z,
-                        msg.linear_acceleration.x, msg.linear_acceleration.y, msg.linear_acceleration.z]
-            imu_data.append(imu_meas)
+        topic_list = ['/imu/data',
+                      '/vrpn_client_node/Leo/pose',
+                      '/vrpn/Leo/pose',
+                      '/baro/data',
+                      '/sonar/data',
+                      '/is_flying',
+                      '/gps/data',
+                      '/mag/data',
+                      '/camera/depth/image_rect',
+                      '/camera/rgb/image_rect_mono/compressed']
 
-        if topic == '/vrpn_client_node/Leo/pose' or topic == '/vrpn/Leo/pose':
-            truth_meas = [msg.header.stamp.to_sec(),
-                          msg.pose.position.z, -msg.pose.position.x, -msg.pose.position.y,
-                          -msg.pose.orientation.w, -msg.pose.orientation.z, msg.pose.orientation.x, msg.pose.orientation.y]
-            truth_pose_data.append(truth_meas)
+        for topic, msg, t in tqdm(bag.read_messages(topics=topic_list), total=bag.get_message_count(topic_list)):
+            if topic == '/imu/data':
+                imu_meas = [msg.header.stamp.to_sec(),
+                            msg.angular_velocity.x, msg.angular_velocity.y, msg.angular_velocity.z,
+                            msg.linear_acceleration.x, msg.linear_acceleration.y, msg.linear_acceleration.z]
+                imu_data.append(imu_meas)
 
-        if topic == '/camera/rgb/image_raw/compressed':
-            np_arr = np.fromstring(msg.data, np.uint8)
-            image = cv2.imdecode(np_arr, cv2.IMREAD_GRAYSCALE)
-            image_time.append(msg.header.stamp.to_sec())
-            image_data.append(image)
+            if topic == '/vrpn_client_node/Leo/pose' or topic == '/vrpn/Leo/pose':
+                truth_meas = [msg.header.stamp.to_sec(),
+                              msg.pose.position.z, -msg.pose.position.x, -msg.pose.position.y,
+                              -msg.pose.orientation.w, -msg.pose.orientation.z, msg.pose.orientation.x, msg.pose.orientation.y]
+                truth_pose_data.append(truth_meas)
+
+            if topic == '/camera/rgb/image_rect_mono/compressed':
+                np_arr = np.fromstring(msg.data, np.uint8)
+                image = cv2.imdecode(np_arr, cv2.IMREAD_GRAYSCALE)
+                image_time.append(msg.header.stamp.to_sec())
+                image_data.append(image)
 
 
 
-        if topic == '/camera/depth/image/compressedDepth':
-            # https://stackoverflow.com/questions/41051998/ros-compresseddepth-to-numpy-or-cv2
-            # https://github.com/ros-perception/image_transport_plugins/blob/indigo-devel/compressed_depth_image_transport/src/codec.cpp
-            raw = cv2.imdecode(np.fromstring(msg.data[12:], np.uint8), cv2.IMREAD_UNCHANGED)
-            fmt, a, b = struct.unpack('iff', msg.data[:12])
+            if topic == '/camera/depth/image_rect':
+                img = np.fromstring(msg.data, np.float32).reshape(msg.height, msg.width)
 
-            scaled = a / (raw.astype(np.float32) - b)
-            scaled[raw == 0] = 0
+                depth_time.append(msg.header.stamp.to_sec())
+                depth_data.append(img)
 
-            depth_time.append(msg.header.stamp.to_sec())
-            depth_data.append(scaled)
+        tmp_dict = dict()
+        tmp_dict['imu_data'] = np.array(imu_data)
+        tmp_dict['truth_pose_data'] = np.array(truth_pose_data)
+        tmp_dict['image_data'] = np.array(image_data)
+        tmp_dict['depth_data'] = np.array(depth_data)
+        tmp_dict['image_time'] = np.array(image_time)
+        tmp_dict['depth_time'] = np.array(depth_time)
+        cPickle.dump(tmp_dict, open('data/bag_tmp.pkl', 'wb'), protocol=2)
+    else:
+        tmp_dict = cPickle.load(open('data/bag_tmp.pkl', 'rb'))
 
-    imu_data = np.array(imu_data)
-    truth_pose_data = np.array(truth_pose_data)
-    image_data = np.array(image_data)
-    depth_data = np.array(depth_data)
-    image_time = np.array(image_time)
-    depth_time = np.array(depth_time)
+    imu_data = tmp_dict['imu_data']
+    truth_pose_data = tmp_dict['truth_pose_data']
+    image_data = tmp_dict['image_data']
+    depth_data = tmp_dict['depth_data']
+    image_time = tmp_dict['image_time']
+    depth_time = tmp_dict['depth_time']
+
 
     # assert np.abs(truth_pose_data[0, 0] - imu_data[0, 0]) < 1e5, 'truth and imu timestamps are vastly different: {} (truth) vs. {} (imu)'.format(truth_pose_data[0, 0], imu_data[0, 0])
 
@@ -178,7 +187,9 @@ def load_data(filename, start=0, end=np.inf, sim_features=False, show_image=Fals
     q_b_c = Quaternion.from_R(np.array([[0, 1, 0],
                                         [0, 0, 1],
                                         [1, 0, 0]]))
-    p_b_c = np.array([[0.16, -0.05, 0.1]]).T
+    # q_b_c = Quaternion.Identity()
+    # p_b_c = np.array([[0.16, -0.05, 0.1]]).T
+    p_b_c = np.zeros((3,1))
 
     if sim_features:
         # Simulate Landmark Measurements
@@ -210,28 +221,43 @@ def load_data(filename, start=0, end=np.inf, sim_features=False, show_image=Fals
         feat_time, zetas, depths, ids = add_landmark(ground_truth, landmarks, p_b_c, q_b_c)
 
     else:
-        tracker = klt_tracker.KLT_tracker(25)
+        tracker = klt_tracker.KLT_tracker(50, show_image=True)
+        lambdas, depths, ids, feat_time = [], [], [], []
 
         _, image_height, image_width = image_data.shape
         _, depth_height, depth_width = depth_data.shape
 
-        lambdas, depths, ids, feat_time = [], [], [], []
+        assert image_height == depth_height and image_width == depth_width
 
         for i, image in enumerate(image_data):
             frame_lambdas, frame_ids = tracker.load_image(image)
 
+            # KLT tracker doesn't consider image bounds :(
+            frame_lambdas = np.clip(frame_lambdas[:, 0], [0, 0], image_data[0].T.shape)
+
             frame_depths = []
             nearest_depth = np.abs(depth_time - image_time[i]).argmin()
-            for y, x in frame_lambdas[:, 0]:
-                dx = (x / float(image_width))*depth_width
-                dy = (y / float(image_height))*depth_height
+            for y, x in frame_lambdas:
+                try:
+                    d = depth_data[nearest_depth][x, y]
+                except IndexError as e:
+                    debug = 1
+                frame_depths.append(d)
 
-                d = bilinear_interpolate(depth_data[nearest_depth], dx, dy)
-
-                frame_depths.append(d if d > 0 else np.nan)
+            # plt.ion()
+            # plt.figure(1, figsize=(10,5))
+            # plt.clf()
+            # plt.subplot(121)
+            # plt.imshow(255 - image, cmap='Greys')
+            # plt.plot(frame_lambdas[:,0], frame_lambdas[:,1], 'mx', markersize=10)
+            # plt.subplot(122)
+            # plt.imshow(depth_data[nearest_depth], cmap='Greys')
+            # plt.plot(frame_lambdas[:, 0], frame_lambdas[:, 1], 'mx', markersize=10)
+            # plt.show()
+            # plt.pause(0.005)
 
             depths.append(np.array(frame_depths)[:,None])
-            lambdas.append(frame_lambdas[:,0])
+            lambdas.append(frame_lambdas)
             ids.append(frame_ids)
             feat_time.append(image_time[i])
 
@@ -249,6 +275,7 @@ def load_data(filename, start=0, end=np.inf, sim_features=False, show_image=Fals
     # if plot_trajectory:
     #     plot_3d_trajectory(ground_truth[:,1:4], ground_truth[:,4:8], qzetas=zetas, depths=depths, p_b_c=p_b_c, q_b_c=q_b_c)
 
+    plt.ioff()
     out_dict = dict()
     out_dict['imu'] = imu_data
     out_dict['truth'] = ground_truth

vi_ekf/vi_ekf.py
@@ -34,6 +34,7 @@ class VI_EKF():
         # 17 main states + 5N feature states
         # pos, vel, att, b_gyro, b_acc, mu, q_feat, rho_feat, q_feat, rho_feat ...
         self.x = x0
+        self.u = np.zeros((6,1))
 
         # Process noise matrix for the 16 main delta states
         self.Qx = np.diag([0.001, 0.001, 0.001,     # pos
@@ -111,6 +112,7 @@ class VI_EKF():
         self.dx = np.zeros((dxZ, 1))
 
         self.use_drag_term = True
+        self.default_depth = np.array([[1.5]])
 
         self.last_propagate = None
         self.initialized = False
@@ -151,6 +153,13 @@ class VI_EKF():
         for i in range(self.len_features):
             qzetas[i,:,None] = self.x[xZ+5*i:xZ+5*i+4]   # 4-vector quaternion
         return qzetas
+
+    def get_camera_state(self):
+        vel = self.x[xVEL:xVEL + 3]
+        omega = self.u[uG:uG+3] - self.x[xB_G:xB_G+3]
+        vel_c_i = self.q_b_c.invrot(vel + skew(omega).dot(self.p_b_c))
+        omega_c_i = self.q_b_c.invrot(omega)
+        return vel_c_i, omega_c_i
 
     # Adds the state with the delta state on the manifold
     def boxplus(self, x, dx):
@@ -193,10 +202,11 @@ class VI_EKF():
         if self.last_propagate is not None:
             # calculate dt from t
             dt = t - self.last_propagate
-            u = np.vstack((y_acc, y_gyro))
+            self.u[uA:uA+3] = y_acc
+            self.u[uG:uG+3] = y_gyro
 
             # Propagate
-            xdot, A, G = self.dynamics(self.x, u)
+            xdot, A, G = self.dynamics(self.x, self.u)
             Pdot = A.dot(self.P) + self.P.dot(A.T) + G.dot(self.Qu).dot(G.T) + self.Qx
             self.x = self.boxplus(self.x, xdot * dt)
             self.P += Pdot*dt
@@ -214,7 +224,7 @@ class VI_EKF():
         # If we haven't seen this feature before, then initialize it
         if measurement_type == 'feat':
             if kwargs['i'] not in self.global_to_local_feature_id.keys():
-                self.init_feature(z, id=kwargs['i'], depth=(kwargs['depth'] if 'depth' in kwargs else np.array([[1.5]])))
+                self.init_feature(z, id=kwargs['i'], depth=(kwargs['depth'] if 'depth' in kwargs else np.nan))
 
         zhat, H = self.measurement_functions[measurement_type](self.x, **kwargs)
 
@@ -234,9 +244,12 @@ class VI_EKF():
 
         # Perform state and covariance update
         if not passive_update:
-            K = self.P.dot(H.T).dot(scipy.linalg.inv(R + H.dot(self.P).dot(H.T)))
-            self.P = (self.I_big - K.dot(H)).dot(self.P)
-            self.x = self.boxplus(self.x, K.dot(residual))
+            try:
+                K = self.P.dot(H.T).dot(scipy.linalg.inv(R + H.dot(self.P).dot(H.T)))
+                self.P = (self.I_big - K.dot(H)).dot(self.P)
+                self.x = self.boxplus(self.x, K.dot(residual))
+            except:
+                print "Nan detected in", measurement_type, "update"
 
         return residual, zhat
 
@@ -247,7 +260,7 @@ class VI_EKF():
         self.x[xB_A:xB_A+3] = b_a
 
     # Used to initialize a new feature.  Returns the feature id associated with this feature
-    def init_feature(self, l, id, depth=None):
+    def init_feature(self, l, id, depth=np.nan):
         assert l.shape == (2, 1) and depth.shape == (1, 1)
 
         self.len_features += 1
@@ -264,6 +277,12 @@ class VI_EKF():
         zeta /= norm(zeta)
         q_zeta = Quaternion.from_two_unit_vectors(self.khat, zeta).elements
 
+        if np.isnan(depth):
+            # Best guess of depth without other information
+            if self.len_features > 0:
+                depth = np.average(1.0/self.x[xZ + 4::5])
+            else:
+                depth = self.default_depth
         self.x = np.vstack((self.x, q_zeta, 1./depth)) # add 5 states to the state vector
 
         # Add three states to the process noise matrix
@@ -367,8 +386,8 @@ class VI_EKF():
         self.G[dxATT:dxATT+3, uG:uG+3] = I_3x3
 
         # Camera Dynamics
-        vel_c_i = self.q_b_c.invrot(vel + skew(omega).dot(self.p_b_c))
         omega_c_i = self.q_b_c.invrot(omega)
+        vel_c_i = self.q_b_c.invrot(vel - skew(omega).dot(self.p_b_c))
 
 
         for i in range(self.len_features):
@@ -395,18 +414,18 @@ class VI_EKF():
             #################################
             ## FEATURE STATE JACOBIAN
             self.A[dxZETA_i:dxZETA_i+2, dxVEL:dxVEL+3] = -rho*T_z.T.dot(skew_zeta).dot(R_b_c)
-            self.A[dxZETA_i:dxZETA_i+2, dxB_G:dxB_G+3] = -T_z.T.dot(rho*skew_zeta.dot(R_b_c).dot(skew_p_b_c) + R_b_c)
-            self.A[dxZETA_i:dxZETA_i+2, dxZETA_i:dxZETA_i+2] = T_z.T.dot(skew(omega_c_i - rho*skew_zeta.dot(vel_c_i)) - (rho*skew_vel_c.dot(skew_zeta))).dot(T_z)
+            self.A[dxZETA_i:dxZETA_i+2, dxB_G:dxB_G+3] = T_z.T.dot(rho*skew_zeta.dot(R_b_c).dot(skew_p_b_c) - R_b_c)
+            self.A[dxZETA_i:dxZETA_i+2, dxZETA_i:dxZETA_i+2] = T_z.T.dot(skew(rho * skew_zeta.dot(vel_c_i) + omega_c_i) - (rho * skew_vel_c.dot(skew_zeta))).dot(T_z)
             self.A[dxZETA_i:dxZETA_i+2, dxRHO_i,None] = -T_z.T.dot(skew_zeta).dot(vel_c_i)
             self.A[dxRHO_i, dxVEL:dxVEL+3] = rho2*zeta.T.dot(R_b_c)
-            self.A[dxRHO_i, dxB_G:dxB_G+3] = rho2*zeta.T.dot(R_b_c).dot(skew_p_b_c)
+            self.A[dxRHO_i, dxB_G:dxB_G+3] = -rho2*zeta.T.dot(R_b_c).dot(skew_p_b_c)
             self.A[dxRHO_i, dxZETA_i:dxZETA_i+2] = -rho2*vel_c_i.T.dot(skew_zeta).dot(T_z)
             self.A[dxRHO_i, dxRHO_i] = 2*rho*zeta.T.dot(vel_c_i).squeeze()
 
             #################################
             ## FEATURE INPUT JACOBIAN
-            self.G[dxZETA_i:dxZETA_i+2, uG:uG+3] = T_z.T.dot(R_b_c + rho*skew_zeta.dot(R_b_c).dot(skew_p_b_c))
-            self.G[dxRHO_i, uG:] = -rho2*zeta.T.dot(R_b_c).dot(skew_p_b_c)
+            self.G[dxZETA_i:dxZETA_i+2, uG:uG+3] = T_z.T.dot(R_b_c - rho*skew_zeta.dot(R_b_c).dot(skew_p_b_c))
+            self.G[dxRHO_i, uG:] = rho2*zeta.T.dot(R_b_c).dot(skew_p_b_c)
 
         return self.dx, self.A, self.G
 
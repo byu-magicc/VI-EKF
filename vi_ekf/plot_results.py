f5ec97228c5b4600772d355653fddcd90aea939b
James Jackson <superjax08@gmail.com>
2018-04-17 15:38:36 -0600

    working attitude only, plotting derivatives too

--- vi_ekf/plot_results.py
+++ vi_ekf/plot_results.py
@@ -25,6 +25,7 @@ perf_file = open(log_dir + str(latest_folder) + "/perf.txt")
 meas_file = open(log_dir + str(latest_folder) + "/meas.txt")
 conf_file = open(log_dir + str(latest_folder) + "/conf.txt")
 input_file = open(log_dir + str(latest_folder) + "/input.txt")
+xdot_file = open(log_dir + str(latest_folder) + "/xdot.txt")
 fig_dir = log_dir + str(latest_folder) + "/plots"
 os.system("mkdir " + fig_dir)
 
@@ -94,6 +95,11 @@ for line in input_file:
     if len(line_arr) < 6: continue
     h.store(line_arr[0], u_acc=line_arr[1:4], u_gyro=line_arr[4:])
 
+for line in xdot_file:
+    line_arr = np.array([float(item) for item in line.split()])
+    if len(line_arr) < 18: continue
+    h.store(line_arr[0], dt=line_arr[1], xdot=line_arr[2:18])
+
 
 h.tonumpy()
 
@@ -127,11 +133,14 @@ start = h.t.xhat[0]
 end = h.t.xhat[-1]
 init_plots(start, end, fig_dir)
 
+# PLOT STATES
 plot_side_by_side(r'$p_{b/n}^n$', 0, 3, h.t.xhat, h.xhat, cov=h.cov if pose_cov else None, truth_t=h.t.pos, truth=h.pos, labels=['p_x', 'p_y', 'p_z'], start_t=start, end_t=end, truth_offset=offset, ylim=[-3, 3])
 plot_side_by_side(r'$p_{b/I}^I$', 0, 3, h.t.global_pos_hat, h.global_pos_hat, cov=h.cov if pose_cov else None, truth_t=h.t.global_pos, truth=h.global_pos, labels=['p_x', 'p_y', 'p_z'], start_t=start, end_t=end, truth_offset=offset, ylim=[-3, 3])
 plot_side_by_side(r'$v_{b/I}^b$', 3, 6, h.t.xhat, h.xhat, cov=h.cov if plot_cov else None, truth_t=v_t, truth=vel_data, labels=['v_x', 'v_y', 'v_z'], start_t=start, end_t=end, truth_offset=offset, ylim=[-3, 3])
 plot_side_by_side(r'$q_n^b$', 6, 10, h.t.xhat, h.xhat, cov=None, truth_t=h.t.att, truth=h.att, labels=['q_w','q_x', 'q_y', 'q_z'], start_t=start, end_t=end, truth_offset=offset)
 plot_side_by_side(r'$q_I^b$', 0, 4, h.t.global_att_hat, h.global_att_hat, cov=None, truth_t=h.t.global_att, truth=h.global_att, labels=['q_w','q_x', 'q_y', 'q_z'], start_t=start, end_t=end, truth_offset=offset)
+
+## PLOT EULER ANGLES
 # Convert relative attitude to euler angles
 true_euler, est_euler = np.zeros((len(h.att),3)), np.zeros((len(h.xhat),3))
 for i, true_quat in enumerate(h.att): true_euler[i,:,None] = Quaternion(true_quat[:,None]).euler
@@ -142,14 +151,26 @@ true_euler, est_euler = np.zeros((len(h.global_att),3)), np.zeros((len(h.global_
 for i, true_quat in enumerate(h.global_att): true_euler[i,:,None] = Quaternion(true_quat[:,None]).euler
 for i, est_quat in enumerate(h.global_att_hat): est_euler[i,:,None] = (Quaternion(est_quat[:,None]).euler)
 plot_side_by_side('global_euler', 0, 3, h.t.global_att_hat, est_euler, truth_t=h.t.global_att, truth=true_euler, start_t=start, end_t=end, labels=[r'\phi', r'\theta', r'\psi'], truth_offset=offset)
-# Filter accelerometer
+
+
+# PLOT INPUTS AND MEASUREMENTS
 b_acc, a_acc = scipy.signal.butter(6, 0.05)
 acc_smooth = scipy.signal.filtfilt(b_acc, a_acc, h.acc, axis=0)
 plot_side_by_side('$y_{a}$', 0, 2, h.t.acc, acc_smooth, truth=h.acc, truth_t=h.t.acc, labels=[r'y_{a,x}', r'y_{a,y}'], start_t=start, end_t=end, truth_offset=offset)
 plot_side_by_side('$y_{alt}$', 0, 1, h.t.alt, h.alt_hat[:,None], truth=h.alt[:,None], truth_t=h.t.alt, labels=[r'-p_z'], start_t=start, end_t=end)
-plot_side_by_side('Bias Terms', 10, 17, h.t.xhat, h.xhat, labels=[r'\beta_{a,x}', r'\beta_{a,y}', r'\beta_{a,z}', r'\beta_{\omega,x}', r'\beta_{\omega,y}', r'\beta_{\omega,z}', 'b'], start_t=start, end_t=end, cov=h.cov if plot_cov else None, cov_bounds=(9,16), truth_offset=offset, ylim=[-0.3, 0.3])
+plot_side_by_side(r'$\beta_{a}$', 10, 13, h.t.xhat, h.xhat, labels=[r'\beta_{a,x}', r'\beta_{a,y}', r'\beta_{a,z}'], start_t=start, end_t=end, cov=h.cov if plot_cov else None, cov_bounds=(9,12), truth_offset=offset)
+plot_side_by_side(r'$\beta_{\omega}$', 13, 16, h.t.xhat, h.xhat, labels=[r'\beta_{\omega,x}', r'\beta_{\omega,y}', r'\beta_{\omega,z}'], start_t=start, end_t=end, cov=h.cov if plot_cov else None, cov_bounds=(12,15), truth_offset=offset)
+plot_side_by_side('drag', 16, 17, h.t.xhat, h.xhat, labels=['b'], start_t=start, end_t=end, cov=h.cov if plot_cov else None, cov_bounds=(15,16), truth_offset=offset)
 plot_side_by_side('u_acc', 0, 3, h.t.u_acc, h.u_acc, labels=[r'u_{a,x}',r'u_{a,y}',r'u_{a,z}'], start_t=start, end_t=end)
 plot_side_by_side('u_gyro', 0, 3, h.t.u_gyro, h.u_gyro, labels=[r'u_{\omega,x}',r'u_{\omega,y}',r'u_{\omega,z}'], start_t=start, end_t=end)
+
+# PLOT DERIVATIVES
+plot_side_by_side(r'$\dot{p}_{b/n}^n$', 0, 3, h.t.xdot, h.xdot, labels=[r'\dot{p}_x', r'\dot{p}_y', r'\dot{p}_z'], start_t=start, end_t=end, truth_offset=offset)
+plot_side_by_side(r'$\dot{v}_{b/I}^b$', 3, 6, h.t.xdot, h.xdot, labels=[r'\dot{v}_x', r'\dot{v}_y', r'\dot{v}_z'], start_t=start, end_t=end, truth_offset=offset)
+plot_side_by_side(r'$\dot{q}_{b/I}$', 6, 9, h.t.xdot, h.xdot, labels=[r'\dot{q}_x', r'\dot{q}_y', r'\dot{q}_z'], start_t=start, end_t=end, truth_offset=offset)
+plot_side_by_side(r'$\dot{\beta}_{a}$', 9, 12, h.t.xdot, h.xdot, labels=[r'\dot{\beta}_{a,x}', r'\dot{\beta}_{a,y}', r'\dot{\beta}_{a,z}'], start_t=start, end_t=end, truth_offset=offset)
+plot_side_by_side(r'$\dot{\beta}_{\omega}$', 12, 15, h.t.xdot, h.xdot, labels=[r'\dot{\beta}_{\omega,x}', r'\dot{\beta}_{\omega,y}', r'\dot{\beta}_{\omega,z}'], start_t=start, end_t=end, truth_offset=offset)
+plot_side_by_side(r'$\dot{b}$', 15, 16, h.t.xdot, h.xdot, labels=[r'\dot{b}'], start_t=start, end_t=end, truth_offset=offset)
 
 # print Final States for baiases for tuning
 print "\nFinal bias States"
@@ -169,8 +190,8 @@ for i in tqdm(ids):
                       truth=h.feat[i], labels=['u', 'v'], start_t=start, end_t=end, truth_offset=None, subdir='lambda')
     if hasattr(h, 'depth') and hasattr(h, 'depth_hat'): 
         plot_side_by_side('x_{}'.format(i), 0, 1, h.t.depth_hat[i], h.depth_hat[i][:, None], truth_t=h.t.depth[i],
-                truth=h.depth[i][:, None], labels=[r'\frac{1}{\rho}'], start_t=start, end_t=end,
-                cov=h.depth_cov[i] if plot_cov else None, truth_offset=None, subdir='rho')
+                      truth=h.depth[i][:, None], labels=[r'\frac{1}{\rho}'], start_t=start, end_t=end,
+                      cov=h.depth_cov[i] if plot_cov else None, truth_offset=None, subdir='rho')
 
 
 

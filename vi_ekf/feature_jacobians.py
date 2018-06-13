b37535c5cb70d7cd7351919d7a372184d01f202a
James Jackson <superjax08@gmail.com>
2018-06-13 17:05:55 -0600

    got almost all the distortion parameters solved

--- vi_ekf/feature_jacobians.py
+++ vi_ekf/feature_jacobians.py
@@ -4,6 +4,8 @@ from __future__ import print_function
 import numpy as np
 from pyquat import *
 import scipy.linalg
+import sys
+from math_helper import T_zeta, q_feat_boxminus, q_feat_boxplus
 
 RST   = "\x1B[0m"
 KRED  = "\x1B[31m"
@@ -48,13 +50,78 @@ def J_pi_inv(eta, Kinv):
     I = np.eye(3)
     return ((I * nv - v.dot(v.T)/nv)/(nv*nv)).dot(Kinv).dot(I[:,:2])
 
+def qz(zeta):
+    skk = skew(khat)
+    return Quaternion.exp(np.arccos(khat.T.dot(zeta)) * skk.dot(zeta)/norm(skk.dot(zeta)))
+
+def f_dyn(zeta, w, p, v, dt):
+    qzeta = qz(zeta)
+    return (q_feat_boxplus(qzeta, (-T_zeta(qzeta).T.dot(w + p*skew(zeta).dot(v))*dt))).R.T.dot(khat)
+
+def jac_dyn(zeta, w, p, v, dt):
+    return -skew(f_r(zeta, w, p, v, dt)) #.dot(jac_m(zeta)).dot(jac_r(zeta, w, p, v, dt))
+    # return qz(zeta).R.dot(jac_r(zeta, w, p, v, dt))
+
+def f_l(zeta):
+    return (skew(khat).dot(zeta))/norm(skew(khat).dot(zeta))
+
+def jac_l(zeta):
+    v = skew(khat).dot(zeta)
+    nv = norm(v)
+    return ((np.eye(3) * nv - v.dot(v.T)/nv)/(nv*nv)).dot(skew(khat))
+
+def f_s(zeta):
+    return khat.T.dot(zeta)
+
+def jac_s(zeta):
+    return khat.T.dot(np.eye(3))
+
+
+def f_m(zeta):
+    return np.arccos(f_s(zeta)) * f_l(zeta)
+
+def jac_m(zeta):
+    s = f_s(zeta)
+    return -1.0/(1.-(s*s))**0.5 * f_l(zeta).dot(jac_s(zeta)) + np.arccos(s)*jac_l(zeta)
+
+def f_delta(zeta, w, rho, v, dt):
+    p = f_p(zeta, w)
+    return (p - (w + rho*skew(zeta).dot(v)))*dt
+
+def jac_delta(zeta, w, p, v, dt):
+    dpdz = jac_p(zeta,w)
+    return (dpdz + p * skew(v))*dt
+
+def f_p(zeta, w):
+    return zeta.dot(zeta.T).dot(w)
+
+def f_r(zeta, w, rho, v, dt):
+    return Quaternion.exp(f_delta(zeta, w, rho, v, dt)).invrot(khat)
+
+def jac_r(zeta, w, rho, v, dt):
+    return skew(khat).dot(jac_delta(zeta, w, rho, v, dt))
+
+
+
+
+def jac_p(zeta, w):
+    out = np.zeros((3,3))
+    for i in range(3):
+        V = np.zeros((3,3))
+        V[i,:] = zeta[:,0]
+        V[:,i] += zeta[:,0]
+        out[:,i,None] = V.dot(w)
+    return out
+
+
 def eq_test(name, x, y, dec=8):
-    print(FGRN("[" + name + "]: "), end='')
+    print(FCYN("[ " + name + " ]: "), end='')
     np.testing.assert_almost_equal(x, y, decimal=dec)
     print(FGRN("PASSED"))
+    sys.stdout.flush()
 
 def jac_test(name, fun, in_dim, x0, analytical, args=[], e=1e-8, dec=5):
-    print(FGRN("[" + name + "]: "), end='')
+    print(FCYN("[ " + name + " ]: "), end='')
     fd = np.zeros_like(analytical)
     I = np.eye(in_dim)
     y0 = fun(x0, *args)
@@ -64,6 +131,7 @@ def jac_test(name, fun, in_dim, x0, analytical, args=[], e=1e-8, dec=5):
 
     np.testing.assert_almost_equal(analytical, fd, decimal=dec, err_msg=name)
     print(FGRN("PASSED"))
+    sys.stdout.flush()
 
 
 if __name__ == '__main__':
@@ -79,9 +147,33 @@ if __name__ == '__main__':
     eq_test("Proj Function", f_pi_inv(f_pi(zeta,K), Kinv), zeta)
     eq_test("Inv Proj Function", f_pi(f_pi_inv(eta,Kinv), K), eta)
 
+    # Check jacobians of projection functions
+    # jac_test("Proj Jacobian", f_pi, 3, zeta, J_pi(zeta,K), args=[K])
+    # jac_test("Inv Proj Jacobian", f_pi_inv, 2, eta, J_pi_inv(eta,K), args=[K])
 
-    jac_test("Proj Jacobian", f_pi, 3, zeta, J_pi(zeta,K), args=[K])
-    jac_test("Inv Proj Jacobian", f_pi_inv, 2, eta, J_pi_inv(eta,K), args=[K])
+    # Check that with no inputs the dynamics are static
+    w = v = np.zeros((3,1))
+    p = 1.0
+    dt = 0.001
+    eq_test("dynamics - freeze", f_dyn(zeta, w, p, v, dt), zeta)
+
+    jac_test("l(zeta)", f_l, 3, zeta, jac_l(zeta))
+    jac_test("s(zeta)", f_s, 3, zeta, jac_s(zeta))
+    jac_test("m(zeta)", f_m, 3, zeta, jac_m(zeta))
+
+    # v = np.random.random((3,1))
+    # w = np.random.random((3,1))
+    jac_test("p(zeta)", f_p, 3, zeta, jac_p(zeta, w), args=[w])
+    jac_test("del(zeta)", f_delta, 3, zeta, jac_delta(zeta, w, p, v, dt), args=[w, p, v, dt])
+    jac_test("r(zeta)", f_r, 3, zeta, jac_r(zeta, w, p, v, dt), args=[w, p, v, dt])
+
+    eq_test("r_eq", f_r(zeta, w, p, v, dt), Quaternion.exp(-(np.eye(3) - zeta.dot(zeta.T)).dot(w + p*skew(zeta).dot(v)) * dt).R.dot(khat))
+    eq_test("dyn_eq", f_dyn(zeta, w, p, v, dt), qz(zeta).R.T.dot(f_r(zeta, w, p, v, dt)), dec=2)
+    jac_test("dyn", f_dyn, 3, zeta, jac_dyn(zeta, w, p, v, dt), args=[w, p, v, dt])
+
+
+
+
 
 
 

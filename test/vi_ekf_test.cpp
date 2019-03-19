448ef274473779f54f6de33f2eff4f82edd75a50
Jerel <jerel.nielsen@gmail.com>
2019-03-19 13:26:02 -0600

    sim test runs but doesn't log truth yet

--- test/vi_ekf_test.cpp
+++ test/vi_ekf_test.cpp
@@ -3,6 +3,7 @@
 #include <gtest/gtest.h>
 #include "multirotor_sim/utils.h"
 #include "multirotor_sim/simulator.h"
+#include "multirotor_sim/estimator_wrapper.h"
 #include "vi_ekf.h"
 
 using namespace std;
@@ -16,8 +17,35 @@ TEST(VIEKF_Test, SimulatedData)
   // Initialize simulation
   Simulator multirotor(true);
   multirotor.load("../params/sim_params.yaml");
-//  multirotor.register_estimator(this);
 
-  // Run the simulation
-  multirotor.run();
+  // Register callbacks
+  EstimatorWrapper est;
+
+  auto imu_cb = [&ekf](const double& t, const Vector6d& z, const Matrix6d& R)
+  {
+    ekf.add_measurement(t, z, vi_ekf::VIEKF::ACC, R, true);
+  };
+  auto feat_cb = [&ekf](const double& t, const multirotor_sim::ImageFeat& z, const Matrix2d& R_pix, const Matrix1d& R_depth)
+  {
+    for (int i = 0; i < z.pixs.size(); ++i)
+      ekf.add_measurement(t, z.pixs[i], vi_ekf::VIEKF::FEAT, R_pix, true, z.feat_ids[i]);
+  };
+//  auto mocap_cb = [&ekf](const double& t, const Xformd& z, const Matrix6d& R)
+//  {
+//    ekf.add_measurement(t, z.t(), vi_ekf::VIEKF::POS, R.topLeftCorner<3,3>(), true);
+//    ekf.add_measurement(t, z.q().elements(), vi_ekf::VIEKF::ATT, R.bottomRightCorner<3,3>(), true);
+//  };
+
+  est.register_imu_cb(imu_cb);
+  est.register_feat_cb(feat_cb);
+//  est.register_mocap_cb(mocap_cb);
+  multirotor.register_estimator(&est);
+
+  // Run the simulation and log information for plotting
+  while (multirotor.run())
+  {
+    //
+  }
+
+  // Check error magnitudes
 }
